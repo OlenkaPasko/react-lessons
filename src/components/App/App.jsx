dcd487381cd5f8e788f399e78f00f109a13eb68a
OlenkaPasko <lena_nechipurenko@ukr.net>
2024-06-13 01:05:45 +0300

    Officer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,21 @@
 import AppBar from "../AppBar/AppBar";
+import OfficerProfile from "../OfficerProfile/Officerprofile";
+
+const firstPilot = {
+  id: 1,
+  name: "Jacob Mercer",
+  rank: "Commander",
+  age: 42,
+  spec: "Fighter Pilot",
+  skills: ["Combat Maneuvers", "Navigation", "Strategic Planning"],
+  status: "Active",
+};
 
 export default function App() {
   return (
     <div>
       <AppBar />
+      <OfficerProfile />
     </div>
   );
 }
---
+++ src/components/OfficerProfile/Officerprofile.jsx
@@ -0,0 +1,13 @@
+
+
+export default function OfficerProfile() {
+    return (
+      <div>
+        <p>Name:Jacob Mercer</p>
+        <p>Rank:Commander</p>
+        <p>Age:42</p>
+        <p>Spec:Fighter Pilot</p>
+        <p>Status:Active</p>
+      </div>
+    );
+}
