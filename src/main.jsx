5b50539872b1345d00c9b5ac91d11153b2d991d2
OlenkaPasko <lena_nechipurenko@ukr.net>
2024-06-12 07:57:37 +0300

    start

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+//import App from "./App.jsx";
+//import './index.css'
+const App = () => {
+  return <p>Hello world</p>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  //<React.StrictMode>
+    <App />
+  //</React.StrictMode>,
+)
