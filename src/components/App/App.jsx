7a8125f6e4646a3827b234394c078c6c0289b530
OlenkaPasko <lena_nechipurenko@ukr.net>
2024-06-18 07:47:33 +0300

    css.module

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,10 @@
+import { FaBeer } from "react-icons/fa";
 import AppBar from "../AppBar/AppBar";
 import officers from "../../components/officers.json";
 import OfficerList from "../OfficerList/OfficerList";
+
+import css from "./App.module.css"
+import { FaMusic } from "react-icons/fa";
 
 //json файл імпортувати тільки в App.jsx
 //console.log(officers);
@@ -10,8 +14,12 @@ export default function App() {
   return (
     //якщо або - або, то використовується тернарний оператор //можна так робити або так
     //коли колекція перебирається map,передається обов'язково ключ key,щоб ідетифікувати для реакту де цей елемент
-    <div>
-      <AppBar/>
+    <div className={css.container}>
+      <AppBar />
+      <h1>
+        React <FaBeer size={20} />
+      </h1>
+      <FaMusic className={css.myIcon} />
       {/*{isModalOpen ? <p>Modal is open</p> : <p>Model closed</p>}
       <p>{isModalOpen ? <p>Modal is open</p> : <p>Model closed</p>}</p>*/}
       {/* <AppBar /> */}
